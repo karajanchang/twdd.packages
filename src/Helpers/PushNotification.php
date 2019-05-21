8ea443361eac0ed757c375690a01a848ff56fb24
david <karajanchang@gmail.com>
2019-05-21 10:55:47 +0800

    [PushNotification] body not exists fixed 2

--- src/Helpers/PushNotification.php
+++ src/Helpers/PushNotification.php
@@ -66,8 +66,8 @@ class PushNotification
         $data->action = $this->action;
 
         $service = $this->service->toArray();
-        $data->title = $service['title'];
-        $data->msg = $service['body'];
+        $data->title = isset($service['title']) ? $service['title'] : '';
+        $data->msg = isset($service['body']) ? $service['body'] : '';
         $data->obj = $this->obj;
 
         return $data;
