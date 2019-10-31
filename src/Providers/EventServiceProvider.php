6caebeaa5a244e165315ff64ccfd07cffdee60f1
david <karajanchang@gmail.com>
2019-10-31 15:57:29 +0800

    [TaskDone] 把Event 移到專案 driver.twdd.tw 裡

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -10,10 +10,10 @@ class EventServiceProvider extends ServiceProvider
 
     protected $listen = [
         'Twdd\Events\TaskDoneEvent' => [
-            'Twdd\Listeners\Send15MinutesArriveCouponListener', //--15分未到送優惠卷
-            'Twdd\Listeners\LineUseFirstDiscountListener', //--line的首用
-            'Twdd\Listeners\DriverGoldenReduceListener', //--叩掉使用金牌
-            'Twdd\Listeners\TaskDoneGiveShareCouponListener', //--分享送優惠
+//            'Twdd\Listeners\Send15MinutesArriveCouponListener', //--15分未到送優惠卷
+//            'Twdd\Listeners\LineUseFirstDiscountListener', //--line的首用
+//            'Twdd\Listeners\DriverGoldenReduceListener', //--叩掉使用金牌
+//            'Twdd\Listeners\TaskDoneGiveShareCouponListener', //--分享送優惠
             'Twdd\Listeners\CouponSetUsedListener', //--coupon設為已使用
 
             //---發出推播
