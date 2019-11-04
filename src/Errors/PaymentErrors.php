f2b2c0248c8b337e92ef5dd77ec6ca07ac1ce6dc
david <karajanchang@gmail.com>
2019-11-04 19:10:38 +0800

    add PaymentErrors

---
+++ src/Errors/PaymentErrors.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 2019-05-08
+ * Time: 10:59
+ */
+
+namespace Twdd\Errors;
+
+class PaymentErrors extends ErrorAbstract
+{
+    protected $unit = 'payment';
+
+    /*
+    public function error1000(){
+
+        return trans('twdd::payment.payment_error');
+    }
+    */
+
+}
