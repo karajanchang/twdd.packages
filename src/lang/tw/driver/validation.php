bba91cd18bf9cf49850b25ce6c4f1edf93d3afa4
david <karajanchang@gmail.com>
2019-11-21 17:50:52 +0800

    add driver validation

--- src/lang/tw/driver/validation.php
+++ src/lang/tw/driver/validation.php
@@ -8,6 +8,9 @@ return [
         'DriverEmail' => '夥伴E-Mail',
         'DriverID' => '夥伴序號',
         'DriverPassword' => '夥伴密碼',
+        'DriverAddress' => '地址',
+        'DriverEmergencyName' => '緊急連絡人姓名',
+        'DriverEmergencyPhone' => '緊急連絡人電話',
     ],
 ];
 
