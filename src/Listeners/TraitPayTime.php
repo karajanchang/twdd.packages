8269770202b2ee4d8ede769a772a5bf79d4b817f
david <karajanchang@gmail.com>
2020-02-26 00:26:06 +0800

    [TraitPayTime] 改用Carbon

--- src/Listeners/TraitPayTime.php
+++ src/Listeners/TraitPayTime.php
@@ -1,10 +1,12 @@
 <?php
 namespace Twdd\Listeners;
 
+use Carbon\Carbon;
+
 Trait TraitPayTime{
     private function payTime(){
         $log = $this->task->paylogs->last();
 
-        return !empty($log->created_at) ? $log->created_at->format('Y-m-d H:i') : date('Y-m-d H:i');
+        return !empty($log->created_at) ? Carbon::parse($log->created_at)->format('Y-m-d H:i') : date('Y-m-d H:i');
     }
 }
