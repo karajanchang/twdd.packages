b85de5f827656efb6c0a5849783bdcdacdab2b4a
david <karajanchang@gmail.com>
2020-05-13 14:06:12 +0800

    [MemberService] 加上 profile()

--- src/Helpers/MemberService.php
+++ src/Helpers/MemberService.php
@@ -9,11 +9,41 @@
 namespace Twdd\Helpers;
 
 
-class MemberService
+use Twdd\Facades\TwddCache;
+use Twdd\Repositories\MemberRepository;
+use Twdd\Services\ServiceAbstract;
+use Twdd\Traits\ModelToolTrait;
+
+class MemberService extends ServiceAbstract
 {
+    use ModelToolTrait;
+
     public function register(array $params, $is_verify_mobile = true){
         $register = app()->make(\Twdd\Services\Member\Register::class);
 
         return $register->init($params, $is_verify_mobile);
+    }
+
+    public function profile(int $id, array $columns = ['*'], $clear_cache = false){
+        $repository = app()->make(MemberRepository::class);
+
+        $default_profile = TwddCache::member($id)->MemberProfile()->key('MemberProfile', $id)->get();
+        if(!$default_profile || $clear_cache===true){
+            $default_profile = $repository->profile($id);
+            TwddCache::member($id)->MemberProfile()->key('MemberProfile', $id)->put($default_profile);
+        }
+
+        $all_columns = ['*'];
+        if(count(array_diff($columns, $all_columns))==0){
+
+            return $default_profile;
+        }
+
+        if($this->checkColumnsIsExistsInThisModel($columns, $default_profile)===false){
+
+            return $repository->find($id, $columns);
+        }
+
+        return $default_profile;
     }
 }
--- src/Repositories/MemberRepository.php
+++ src/Repositories/MemberRepository.php
@@ -24,11 +24,26 @@ class MemberRepository extends Repository
     public function countByUserPhone($UserPhone){
         $userPhoneCriteria = new WhereUserPhone($UserPhone);
         $this->pushCriteria($userPhoneCriteria);
+
         return $this->count();
     }
 
     public function byInviteCode(string $OtherInviteCode){
 
         return $this->where('InviteCode', $OtherInviteCode)->first();
+    }
+
+    public function profile(int $id, array $columns = []){
+        $rcolumns = [
+            'member_grade_id', 'UserName', 'UserName', 'UserPhone', 'UserEmail', 'UserGender', 'InviteCode', 'OtherInviteCode',
+            'is_online', 'is_mobile_verify', 'is_registered',
+            'UserEmergencyName', 'UserEmergencyPhone', 'UserEmergencyGender',
+            'nums7', 'numsFail', 'numsBy7', 'numsByFail', 'pay_type',
+        ];
+        if(count($columns)){
+            $rcolumns = $columns;
+        }
+
+        return $this->find($id, $rcolumns);
     }
 }
