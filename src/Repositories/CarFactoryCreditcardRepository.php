55a44e5eba5839e9103782263329249c90b5ea0d
david <karajanchang@gmail.com>
2021-01-12 14:42:38 +0800

    [API-36]【車廠代駕】 - bug修復

--- src/Repositories/CarFactoryCreditcardRepository.php
+++ src/Repositories/CarFactoryCreditcardRepository.php
@@ -4,6 +4,7 @@
 namespace Twdd\Repositories;
 
 
+use Illuminate\Database\Eloquent\Model;
 use Twdd\Models\CarFactoryCreditcard;
 use Zhyu\Repositories\Eloquents\Repository;
 
@@ -14,7 +15,7 @@ class CarFactoryCreditcardRepository extends Repository
         return CarFactoryCreditcard::class;
     }
 
-    public function findByTask(int $task){
+    public function findByTask(Model $task){
         $models = $this->findWhereCache(['car_factory_id' => $task->car_factory_id], ['*'], 'CarFactoryCreditcardRepository'.$task->car_factory_id, 600);
         $model = $models->where('is_default', 1)->first();
         if(!empty($model->id)){
