edc880bf1a25d84a3d08980bf4b32a2d92274e7d
david <karajanchang@gmail.com>
2021-12-02 14:29:01 +0800

    TraitAppVer 打錯字

--- src/Services/Match/CallTypes/Traits/TraitAppVer.php
+++ src/Services/Match/CallTypes/Traits/TraitAppVer.php
@@ -16,7 +16,7 @@ trait TraitAppVer
         $AppVer = !empty($attributes['AppVer']) ? $attributes['AppVer'] : null;
         if(!is_null($DeviceType) && !is_null($AppVer)){
             if($DeviceType=='iphone'){
-                $mini_ver = env('APP_VER_VER_IOS', '3.6.5');
+                $mini_ver = env('APP_MINI_VER_IOS', '3.6.5');
             }else{
                 $mini_ver = env('APP_MINI_VER_ANDROID', '3.4.2');
             }
