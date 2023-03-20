d2c2108c4877a406d07c372db8425cb58bd749e0
ianlin <j135246789@gmail.com>
2023-03-20 12:09:02 +0800

    update file path

--- src/Helpers/InvoiceFactory.php
+++ src/Helpers/InvoiceFactory.php
@@ -5,12 +5,15 @@ namespace Twdd\Helpers;
 
 use Illuminate\Support\Facades\App;
 use Twdd\Services\Invoice\InvoiceInterface;
+use Twdd\Services\Invoice\Type\B2C;
+use Twdd\Services\Invoice\Type\B2B;
+
 class InvoiceFactory
 {
     public function bind(string $invoiceType)
     {
         
-        $className = $invoiceType == "B2C" ? \Twdd\Services\Invoice\Type\B2C::class : \Twdd\Services\Invoice\Type\B2B::class;
+        $className = $invoiceType == "B2C" ? B2C::class : B2B::class;
        
         App::bind(InvoiceInterface::class, $className);
     }
