b93c27b33c28424349bbb111022459d3a76c0b66
ianlin <j135246789@gmail.com>
2023-10-05 15:42:58 +0800

    無駕駛時發送簡訊 & 修正企業簽單任務取消會收費的問題 & 後台客服預約鐘點代駕會開出To B 發票

--- src/Services/Invoice/Type/B2B.php
+++ src/Services/Invoice/Type/B2B.php
@@ -5,6 +5,8 @@ namespace Twdd\Services\Invoice\Type;
 use Twdd\Services\Invoice\AbstractSetting;
 use Twdd\Services\Invoice\InvoiceInterface;
 use Twdd\Repositories\EcpayInvoiceRepository;
+use Twdd\Models\Enterprise;
+use Twdd\Models\CalldriverTaskMap;
 
 class B2B extends AbstractSetting implements InvoiceInterface
 {
@@ -13,7 +15,7 @@ class B2B extends AbstractSetting implements InvoiceInterface
     {
         $this->payment = $payment;
     }
-    
+
     public function issue()
     {
         $url = $this->url . "/issue/B2BInvoice";
@@ -26,8 +28,8 @@ class B2B extends AbstractSetting implements InvoiceInterface
             $id = $this->payment["id"] ?? 'unknown';
             return [
                 'status' => 2,
-                'err'=>$result["error"],
-                'msg' => '發票開立失敗, 來源為B2B帳單:'.$id
+                'err' => $result["error"],
+                'msg' => '發票開立失敗, 來源為B2B帳單:' . $id
             ];
         }
 
@@ -38,52 +40,52 @@ class B2B extends AbstractSetting implements InvoiceInterface
             "taxId" => $this->enterprise->GUI_number,
             "item" => [
                 "itemSeq" => 1,
-                "itemName"=> "軟體交易平台服務費",
-                "itemCount"=> 1,
-                "itemWord"=> "次",
-                "itemPrice"=> round($params['fee']/1.05,0),
-                "itemAmount"=> round($params['fee']/1.05,0)
+                "itemName" => "軟體交易平台服務費",
+                "itemCount" => 1,
+                "itemWord" => "次",
+                "itemPrice" => round($params['fee'] / 1.05, 0),
+                "itemAmount" => round($params['fee'] / 1.05, 0)
             ]
         ];
 
 
-        $result = $this->call($url,$callData);
+        $result = $this->call($url, $callData);
 
         if (!$result['code']) {
 
             $id = $this->payment["id"] ?? 'unknown';
             return [
                 'status' => 2,
-                'err'=>$result["error"],
-                'msg' => '發票開立失敗, 來源為B2B帳單:'.$id
+                'err' => $result["error"],
+                'msg' => '發票開立失敗, 來源為B2B帳單:' . $id
             ];
         }
 
         $invoice = $this->store($result);
-        return ['invoice'=>$invoice,'status'=> 1];
+        return ['invoice' => $invoice, 'status' => 1];
     }
 
     private function maintain()
     {
         $url = $this->url . "/set";
         //因為帳務聯絡人不一定有, 所以需要預設值
-        if (isset($this->enterprise->accountContact->email)){
-            $email = sprintf("%s;%s",$this->enterprise->mainContact->email,$this->enterprise->accountContact->email);
-        }else{
+        if (isset($this->enterprise->accountContact->email)) {
+            $email = sprintf("%s;%s", $this->enterprise->mainContact->email, $this->enterprise->accountContact->email);
+        } else {
             $email = $this->enterprise->mainContact->email;
         }
 
         $setting = [
-            "Identifier" =>$this->enterprise->GUI_number,
-            "CustomerNumber"=>$this->enterprise->GUI_number,
-            "CompanyName"=>$this->enterprise->title,
-            "EmailAddress"=> $email
+            "Identifier" => $this->enterprise->GUI_number,
+            "CustomerNumber" => $this->enterprise->GUI_number,
+            "CompanyName" => $this->enterprise->title,
+            "EmailAddress" => $email
         ];
 
-        return $this->call($url,$setting);
+        return $this->call($url, $setting);
     }
 
-    //B2B作廢發票目前會由財務手動去綠界進行操作, 旦此處先保留
+
     public function invalid()
     {
         $url = $this->url . "/invalid/B2BInvoice";
@@ -91,24 +93,24 @@ class B2B extends AbstractSetting implements InvoiceInterface
 
         $callData = [
             "InvoiceNumber" => $params['model']->ecpay_invoice->invoice_number,
-            "InvoiceDate"=> $params['model']->ecpay_invoice->created_at->format('Y-m-d'),
-            "Reason"=> "鐘點代駕取消服務"
+            "InvoiceDate" => $params['model']->ecpay_invoice->created_at->format('Y-m-d'),
+            "Reason" => "鐘點代駕取消服務"
         ];
 
-        $result = $this->call($url,$callData,'PUT');
+        $result = $this->call($url, $callData, 'PUT');
 
         if (!$result['code']) {
 
             $id = $this->payment["id"] ?? 'unknown';
             return [
                 'status' => 4,
-                'err'=>$result["error"],
-                'msg' => '發票作廢失敗, 發票號碼為:'.$params['model']->ecpay_invoice->invoice_number
+                'err' => $result["error"],
+                'msg' => '發票作廢失敗, 發票號碼為:' . $params['model']->ecpay_invoice->invoice_number
             ];
         }
 
         $this->delete($params['model']->ecpay_invoice->id);
-        return ['status'=>3];
+        return ['status' => 3];
     }
 
     public function store($data)
@@ -119,8 +121,17 @@ class B2B extends AbstractSetting implements InvoiceInterface
             'invoice_number' =>  $data['callback']['InvoiceNo'],
             'invoice_type' => 2,
             'invoice_amount' => $params['fee'],
-            'enterprise_payment_id' => $this->payment["id"] ?? null
         ];
+
+        //乘客打統編也會變成btob, 所以要檢查來源
+        if ($this->payment instanceof CalldriverTaskMap) {
+            $invoiceData['calldriver_task_map_id'] = $this->payment["id"] ?? null;
+            if ($this->task) {
+                $invoiceData['task_id'] = $this->task->id;
+            }
+        } else {
+            $invoiceData['enterprise_payment_id'] = $this->payment["id"] ?? null;
+        }
 
         return app(EcpayInvoiceRepository::class)->create($invoiceData);
     }
